b136ba51bc47f3965dfde61fe989ae5c5d8096bf
Erich Bremer <erich@ebremer.com>
2025-08-14 10:54:32 -0400

    wip

--- Halcyon/src/main/java/com/ebremer/vandegraph/dev/Stack.java
+++ Halcyon/src/main/java/com/ebremer/vandegraph/dev/Stack.java
@@ -14,7 +14,7 @@ import org.springframework.core.io.ClassPathResource;
  * @author erich
  */
 public class Stack {
-    public static String SHACLPATH = "stack.ttl";
+    public static String SHACLPATH = "stack.jsonld";
     private final Model stack = ModelFactory.createDefaultModel();
     final private static Logger logger = LoggerFactory.getLogger(Stack.class);
     
@@ -25,14 +25,14 @@ public class Stack {
             RDFParser.create()
                     .source(cpr.getInputStream())
                     .base(baseURI)
-                    .lang(Lang.TURTLE)
-                    //.lang(Lang.JSONLD11)
+                    //.lang(Lang.TURTLE)
+                    .lang(Lang.JSONLD11)
                     //errorHandler(ErrorHandlerFactory.errorHandlerStrict()) // Strict parsing
                     .parse(stack);
         } catch (IOException ex) {
             System.getLogger(Stack.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
         }
-        //stack.write(System.out, "TTL");
+        stack.write(System.out, "TTL");
     }
     
     public Model getModel() {
