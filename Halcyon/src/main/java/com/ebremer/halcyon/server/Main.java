1ddd194020c5b3a584d6a6b1f3a00ac5b190c05f
Erich Bremer <erich@ebremer.com>
2025-10-24 16:40:47 -0400

    wip

--- Halcyon/src/main/java/com/ebremer/halcyon/server/Main.java
+++ Halcyon/src/main/java/com/ebremer/halcyon/server/Main.java
@@ -139,16 +139,16 @@ public class Main {
         });
         INIT i = new INIT();
         i.init();
-        DataCore dc = DataCore.getInstance();
-        Dataset ds = dc.getDataset();        
-        ds.begin(ReadWrite.WRITE);
-        ds.removeNamedModel("https://localhost:8888/ldp/utah/HnE/Stack2/stack.jsonld");
+   //     DataCore dc = DataCore.getInstance();
+        //Dataset ds = dc.getDataset();        
+        //ds.begin(ReadWrite.WRITE);
+        //ds.removeNamedModel("https://localhost:8888/ldp/utah/HnE/Stack2/stack.jsonld");
         //Stack stack = new Stack();
-        ds.removeNamedModel("https://localhost:8888/utah/HnE/Stack2/stack.jsonld");
-        ds.removeNamedModel("file:///D:/HalcyonStorage/utah/HnE/Stack2/stack.jsonld");
+//        ds.removeNamedModel("https://localhost:8888/utah/HnE/Stack2/stack.jsonld");
+//        ds.removeNamedModel("file:///D:/HalcyonStorage/utah/HnE/Stack2/stack.jsonld");
         //ds.addNamedModel("https://localhost:8888/stack", stack.getModel());
-        ds.commit();
-        ds.end();
+//        ds.commit();
+//        ds.end();
         if (!(System.getProperty("spring.aot.processing") != null)) {
             SPARQLEndPoint.getSPARQLEndPoint();
         }    
